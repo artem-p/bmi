b986fd5567d45f1814e5d63cdce22db6bab4df4a
artem-p <artempugachev1@gmail.com>
2017-03-19 12:15:31 +0300

    bmi test blank

---
+++ app/src/test/java/ru/artempugachev/bmi/PersonTest.java
@@ -0,0 +1,17 @@
+package ru.artempugachev.bmi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Test for bmi computing
+ */
+
+public class PersonTest {
+    @Test
+    public void compute_bmi() {
+        assertEquals(4, 2 + 2);
+    }
+}
