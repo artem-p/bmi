6e769a96aad05d578b69d0c75b98f63d6ed94f7d
artem-p <artempugachev1@gmail.com>
2017-03-23 11:14:05 +0300

    bmi updates after input

--- app/src/main/java/ru/artempugachev/bmi/MainActivity.java
+++ app/src/main/java/ru/artempugachev/bmi/MainActivity.java
@@ -61,39 +61,29 @@ public class MainActivity extends AppCompatActivity {
         mBmiTextView.setText(getString(R.string.bmi, bmi));
     }
 
+
     private void setListeners() {
-        mHeightEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        mWeihtEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+        mHeightEditText.addTextChangedListener(new HeightWeightTextWatcher());
+        mWeihtEditText.addTextChangedListener(new HeightWeightTextWatcher());
     }
+
+
+    private class HeightWeightTextWatcher implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            updateBmi();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    }
+
 }
