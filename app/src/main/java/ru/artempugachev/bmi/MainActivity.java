526d8f2dae0d87bf1a5660aa4a249af5deacc666
artem-p <artempugachev1@gmail.com>
2017-03-23 10:14:23 +0300

    add listeners for inputs

--- app/src/main/java/ru/artempugachev/bmi/MainActivity.java
+++ app/src/main/java/ru/artempugachev/bmi/MainActivity.java
@@ -2,12 +2,67 @@ package ru.artempugachev.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mHeightEditText;
+    private EditText mWeihtEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHeightEditText = (EditText) findViewById(R.id.etHeight);
+        mWeihtEditText = (EditText) findViewById(R.id.etWeight);
+
+        setListeners();
+        updateBmi();
+    }
+
+    /**
+     * Get height and weight values from inputs.
+     * Compute Bmi and display it
+     * */
+    private void updateBmi() {
+//        String sHeight =
+    }
+
+    private void setListeners() {
+        mHeightEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mWeihtEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
