90d7d3bdea05cc02f3bd875870b69c4e75c916e6
artem-p <artempugachev1@gmail.com>
2017-03-30 10:56:30 +0300

    test for saving current vals

--- app/src/androidTest/java/ru/artempugachev/bmi/CurrentValsTest.java
+++ app/src/androidTest/java/ru/artempugachev/bmi/CurrentValsTest.java
@@ -1,5 +1,7 @@
 package ru.artempugachev.bmi;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -7,6 +9,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -26,9 +29,29 @@ public class CurrentValsTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
+
+    /**
+     * Test saving current input vals to preferences
+     * */
     @Test
     public void save_cur_vals_to_prefs() {
+        // input values
         onView(withId(R.id.etHeight)).perform(typeText("178"));
-        // todo make test
+        onView(withId(R.id.etWeight)).perform(typeText("73"));
+        onView(withId(R.id.tvBmi)).check(matches(withText("BMI = 23.0")));
+
+        // save values to preferences
+        MainActivity mainActivity = mActivityRule.getActivity();
+        SharedPreferences preferences = mainActivity.getPreferences(Context.MODE_PRIVATE);
+        mainActivity.saveToPreferences(preferences);
+
+        // check values was saved
+        String height = preferences.getString(mainActivity.getString(R.string.pref_cur_height), "");
+        String weight = preferences.getString(mainActivity.getString(R.string.pref_cur_weight), "");
+        String bmi = preferences.getString(mainActivity.getString(R.string.pref_cur_bmi), "");
+
+        assertEquals(height, "178");
+        assertEquals(weight, "73");
+        assertEquals(bmi, "BMI = 23.0");
     }
 }
